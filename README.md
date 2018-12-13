ea362c4bd26f3bc396df0d39b972fcf0081f8ac9
DTutykhin <dmitry.tutykhin@ibm.com>
2018-12-13 13:26:32 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # srproxy
-Simple Request Proxy
+Get request proxy to.
+
+### Installation:
+
+```
+npm i srproxy
+```
+
+### Usage:
+Run proxy:
+```
+srproxy -s http://postman-echo.com/
+```
+
+Test it with `curl`:
+```
+curl http://0.0.0.0:9989/get?foo=bar
+```
+
+
+### Options: 
+
+`-s, --server <server>` - Server to proxy request to. Default to `http://google.com`
+
+`-h, --host <host>`     - Local hostname to listen on. Default to `0.0.0.0`
+
+`-p, --port <port>`     - Local port to listen on. Default to `9989`
---
+++ index.js
@@ -0,0 +1,50 @@
+const http = require('http');
+const { URL } = require('url');
+const _request = url => new Promise((resolve, reject) => {
+  http.get(url, (res) => {
+    res.setEncoding('utf8');
+    const { statusCode, headers } = res;
+    let body = '';
+    res.on('data', (chunk) => {
+      body += chunk;
+    }).on('end', () => {
+      resolve({ statusCode, headers, body });
+    });
+  }).on('error', reject);
+});
+module.exports = (options = {
+  host: '0.0.0.0',
+  port: 9989,
+  server: 'http://google.com',
+}) => {
+  console.info('Starting server...');
+  const server = http.createServer(async (req, res) => {
+    console.info('Redirecting...');
+    try {
+      const url = new URL(req.url, options.server).toString();
+      const { statusCode, headers, body } = await _request(url);
+      res.writeHead(statusCode, headers);
+      res.write(body);
+      res.end();
+      console.info('Ok');
+    } catch (err) {
+      console.error('Request error', err);
+      res.writeHead(500, err.message);
+      res.write(err.message);
+      res.end();
+    }
+  }).listen(options.port, options.host);
+  const stop = () => {
+    console.info('Stopping server...');
+    server.close(() => process.exit(0));
+    setTimeout(() => {
+      process.exit(0);
+    }, 5000); // Force exit
+  };
+  server.on('listening', () => {
+    console.info(`Redirecting requests to ${options.server}`);
+    console.info(`Listening on http://${options.host}:${options.port}`);
+  });
+  process.on('SIGTERM', stop);
+  process.on('SIGINT', stop);
+};
